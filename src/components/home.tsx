26888ef5ec0fc183a6aea662db9243abb0775c11
KBrand <kbrandn25@gmail.com>
2026-03-26 21:55:05 +0100

    env file removed from the main repo

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <ImgSearch/>
       <h1 className="text-4xl font-bold mb-4">Welcome to the Image Search App</h1>
-      <p className="text-lg text-gray-600">Search for images using the Unsplash API</p>
+       <p className="text-lg text-gray-600">Search for images using the Unsplash API</p>
     </div>
   );
 }
