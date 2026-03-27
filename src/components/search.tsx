dd880e6db16315f986dc3ea48443d7853e648ba8
KBrand <kbrandn25@gmail.com>
2026-03-27 12:19:51 +0100

    created to resolve the problem in the code

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -10,7 +10,7 @@ export default function ImgSearch({ onSearch }: SearchProps) {
   return (
     <div className="flex flex-row items-center py-2 px-6 border-2 border-b-blue-800/70 outline-1 outline-blue-800/50 rounded-3xl my-3 mx-auto max-w-3xl">
       <input type="search" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search for an Image..." className="outline-none border-none w-full text-lg pr-4" />
-      <button className="cursor-pointer" onClick={() => onSearch(search)}>
+      <button className="cursor-pointer" onClick={() => onSearch(search)} >
         <Search size={25} />
       </button>
     </div> 
