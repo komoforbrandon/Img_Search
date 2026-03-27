96ee3fb8ad74a3602aa15307178edb2c424f92ab
Komofor Brandon Nghoneyi <kbrandn25@gmail.com>
2026-03-27 11:49:21 +0100

    Revert "An ImageSearch App where data is fetch using an api and display to the user"

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Routing from './Routes/routing'
 import Sidebar from './components/sidebar'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
 function App() {
-  const [leftsidebar, setLeftsidebar] = useState<boolean>(true)
+const [leftsidebar, setLeftsidebar] = useState<boolean>(true)
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="flex min-h-screen">
-        <Sidebar
-          isLeftbar={leftsidebar}
-          changeIsLeftbar={(value) => setLeftsidebar(value)}
-        />
-        <Routing />
-      </div>
-    </QueryClientProvider>
+    <div className="flex min-h-screen">
+      <Sidebar
+        isLeftbar={leftsidebar}
+        changeIsLeftbar={(value) => setLeftsidebar(value)}
+      />
+      <Routing />
+    </div>
   )
 }
 
--- src/api/imgfetch.ts
+++ src/api/imgfetch.ts
@@ -1,5 +1,7 @@
-export default async function fetchImages(search: string) {
-  const response = await fetch( `https://api.unsplash.com/search/photos?query=${search}&page=6&client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`);
+export default async function fetchImages(query: string) {
+  const response = await fetch(
+    `https://api.unsplash.com/search/photos?query=${query}&page=4&client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`
+  );
   const data = await response.json();
-  return data
+  return data.results;
 }       
