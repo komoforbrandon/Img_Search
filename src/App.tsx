8cd3c7d37cf4d063ce0d89b1eb2d480823c7662b
KBrand <kbrandn25@gmail.com>
2026-03-27 12:46:19 +0100

    some changes made so that the app can work correctly

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import Routing from './Routes/routing'
 import Sidebar from './components/sidebar'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 function App() {
-const [leftsidebar, setLeftsidebar] = useState<boolean>(true)
+  const [leftsidebar, setLeftsidebar] = useState<boolean>(true)
   return (
-    <div className="flex min-h-screen">
-      <Sidebar
-        isLeftbar={leftsidebar}
-        changeIsLeftbar={(value) => setLeftsidebar(value)}
-      />
-      <Routing />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="flex min-h-screen">
+        <Sidebar
+          isLeftbar={leftsidebar}
+          changeIsLeftbar={(value) => setLeftsidebar(value)}
+        />
+        <Routing />
+      </div>
+    </QueryClientProvider>
   )
 }
 
